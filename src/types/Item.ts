226f9d91323c56e684e726e194e87223d78e9a81
JoYoungHyun <younghyun550@gmail.com>
2025-03-13 20:22:25 +0900

    feat: 챔피언 및 아이템 타입 정의 추가

---
+++ src/types/Item.ts
@@ -0,0 +1,31 @@
+export type Item = {
+  name: string;
+  description: string;
+  colloq: string;
+  plaintext: string;
+  from: string[];
+  into: string[];
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  };
+  gold: {
+    base: number;
+    purchasable: boolean;
+    total: number;
+    sell: number;
+  };
+  tags: string[];
+  maps: {
+    [key: number]: boolean;
+  };
+  stats: {
+    FlatHPPoolMod: number;
+  };
+  depth: number;
+};
