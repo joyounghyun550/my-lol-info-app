b8cedcdc09fe5bd302d1a7cf81485a5cf0ea4f6b
JoYoungHyun <younghyun550@gmail.com>
2025-03-13 20:15:52 +0900

    feat: 레이아웃에 헤더와 푸터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "../fonts/GeistVF.woff",
@@ -28,7 +29,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <header className="bg-gray-800 text-white py-4 fixed top-0 w-full z-10">
+          <nav className="container mx-auto flex justify-around">
+            <Link href={"/"}>홈</Link>
+            <Link href={"/champions"}>챔피언 목록</Link>
+            <Link href={"/items"}>아이템 목록</Link>
+            <Link href={"/rotation"}>챔피언 로테이션</Link>
+          </nav>
+        </header>
+        <main className="container mx-auto mt-20">{children}</main>
+        <footer className="bg-gray-800 p-4 mt-8 fixed bottom-0 w-full">
+          <div className="container mx-auto text-center text-white text-sm">
+            리그 오브 레전드 정보 앱
+          </div>
+        </footer>
       </body>
     </html>
   );
